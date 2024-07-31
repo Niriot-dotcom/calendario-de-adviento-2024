bb48a0c5903a59d1c9ce098398747c93de6fa938
Paty Lopez <lopezpatyyarely@gmail.com>
2024-07-31 13:59:56 -0600

    init CSS fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
-      fontFamily: {
-        helvetica: ["Helvetica"],
-        kepler: ["kepler-std-display"],
-        latino: ["latino-gothic-variable"],
-        ivy: ["kepler-std-display"],
-      },
       colors: {
         agreen: "#226f54",
         abeige: "#f2e8cf",
