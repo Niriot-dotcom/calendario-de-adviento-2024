fdc68450409bca91e4942cf04c656132db2b630a
Paty Lopez <lopezpatyyarely@gmail.com>
2024-08-07 09:36:41 -0600

    delete future pages for deployment purposes.

--- src/routes/_+page.server.ts
+++
@@ -1,5 +0,0 @@
-import Page from "./+page.svelte";
-import { getServerState } from "$lib";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = ({ url }) => getServerState(Page, url);
