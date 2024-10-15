c6c1f0eec59d9b1e0f98feeee42787d4f95df6fa
Paty Lopez <lopezpatyyarely@gmail.com>
2024-10-15 09:23:06 -0600

    fix WhiteLogo width on Safari.

--- src/lib/components/WhiteLogo.svelte
+++ src/lib/components/WhiteLogo.svelte
@@ -2,7 +2,7 @@
 <div class="z-50 absolute top-0 right-10">
   <div class="h-[10vh] md:h-[10vh] z-10">
     <img
-      class="w-full h-full opacity-70"
+      class="w-auto h-full opacity-70 object-contain"
       alt="LOGO VIANNEY INVIERNO"
       src="/images/ELEMENTOS/LOGO-BLANCO.webp"
     />
