b3b4ffc0ba557accd0643fe66da9a65700cd2efd
Paty Lopez <lopezpatyyarely@gmail.com>
2024-10-15 09:21:01 -0600

    firebase configuration.

---
+++ src/lib/constants/db.ts
@@ -0,0 +1,12 @@
+export const COLLECTIONS = {
+  Registro: "adviento_whatsapp",
+  Usuarios: "adviento_usuarios",
+};
+
+export type USER_SCHEMA = {
+  username: string;
+  email: string;
+  angelitaName: string;
+  angelitoName: string;
+  lastDay: number;
+};
