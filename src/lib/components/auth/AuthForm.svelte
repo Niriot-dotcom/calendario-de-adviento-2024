40b15f782fd8338be42cf5884fc9ed9768598c57
Paty Lopez <lopezpatyyarely@gmail.com>
2024-10-15 22:02:37 -0600

    AuthForm with errorMsg, loader and fix component wrapper.

--- src/lib/components/auth/AuthForm.svelte
+++ src/lib/components/auth/AuthForm.svelte
@@ -8,6 +8,7 @@
   let error = false;
   let register = false;
   let authenticating = false;
+  let errorMsg = "Hubo un error, por favor verifica tus datos";
 
   async function handleAuthenticate() {
     if (authenticating) {
@@ -30,7 +31,11 @@
         window.location.href = register ? "/nombramiento" : "/inicio";
       }
     } catch (err) {
-      console.log("There was an auth error", err);
+      console.log("err.code: ", err.code);
+      if (err.code === "auth/invalid-login-credentials") {
+        errorMsg = "Verifica tu correo o contraseña";
+      }
+
       error = true;
       authenticating = false;
     }
@@ -38,156 +43,130 @@
 
   function handleRegister() {
     register = !register;
+    error = false;
+    authenticating = false;
+    errorMsg = "Hubo un error, por favor verifica tus datos";
   }
 </script>
 
-<!-- <button
-  class="px-5 bg-white rounded-xl w-1/2 h-10 text-nowrap flex space-x-2 items-center"
->
-  <img class="w-7 h-7" alt="GOOGLE ICON" src="/images/ICONOS/google.webp" />
-  <span class="f6-latino">Usar Google</span>
-</button> -->
-<!-- <button
-  class="px-5 bg-white rounded-xl w-1/2 h-10 text-nowrap flex space-x-2 items-center"
->
-  <img class="w-7 h-7" alt="FACEBOOK ICON" src="/images/ICONOS/facebook.png" />
-  <span class="f6-latino">Usar Facebook</span>
-</button> -->
-
-<!-- EMAIL AND PASSWORD -->
-<Dialog.Root>
-  <Dialog.Trigger
-    class="px-5 bg-white rounded-xl w-1/2 h-10 text-nowrap flex space-x-2 items-center"
+<!-- TODO -->
+<div class="text-agreen flex flex-col space-y-1 mt-0 items-center w-full">
+  <button
+    class="px-5 bg-white rounded-xl w-1/2 h-8 text-nowrap flex space-x-2 items-center"
   >
-    <img class="w-7 h-7" alt="EMAIL ICON" src="/images/ICONOS/email.webp" />
-    <span class="f6-latino">Usar mi correo</span>
-  </Dialog.Trigger>
-
-  <Dialog.Content
-    class="bg-white flex flex-col justify-center items-center w-10/12 rounded-xl border-0"
-    style="background: url('/images/FONDOS/FONDO.webp') no-repeat;"
+    <img
+      class="w-[2.5vh] h-[2.5vh]"
+      alt="GOOGLE ICON"
+      src="/images/ICONOS/google.webp"
+    />
+    <span class="f6-latino">Usar Google</span>
+  </button>
+  <button
+    class="px-5 bg-white rounded-xl w-1/2 h-8 text-nowrap flex space-x-2 items-center"
   >
-    <Dialog.Header>
-      <Dialog.Title class="text-agreen f6-latino">
-        {register ? "Registro" : "Iniciar sesión"}
-      </Dialog.Title>
-      <Dialog.Description>
-        {#if error}
-          <p class="error">Tus credenciales no son correctas.</p>
-        {/if}
-      </Dialog.Description>
-    </Dialog.Header>
+    <img
+      class="w-[2.5vh] h-[2.5vh]"
+      alt="FACEBOOK ICON"
+      src="/images/ICONOS/facebook.png"
+    />
+    <span class="f6-latino">Usar Facebook</span>
+  </button>
 
-    <div class="w-11/12 md:w-8/12">
-      <form class="flex flex-col space-y-2 items-center">
-        <label class="w-full">
-          <p class="text-agreen f10-latino">Correo electrónico</p>
-          <input
-            bind:value={email}
-            type="email"
-            placeholder="Correo electrónico"
-            class="vinput"
-          />
-        </label>
+  <!-- EMAIL AND PASSWORD -->
+  <Dialog.Root>
+    <Dialog.Trigger
+      class="px-5 bg-white rounded-xl w-1/2 h-8 text-nowrap flex space-x-2 items-center"
+    >
+      <img
+        class="w-[2.5vh] h-[2.5vh]"
+        alt="EMAIL ICON"
+        src="/images/ICONOS/email.webp"
+      />
+      <span class="f6-latino">Usar mi correo</span>
+    </Dialog.Trigger>
 
-        <label class="w-full">
-          <p class=" text-agreen f10-latino">Contraseña</p>
-          <input
-            bind:value={password}
-            type="password"
-            placeholder="Contraseña"
-            class="vinput focus:ring-2 focus:ring-ared"
-          />
-        </label>
-        {#if register}
-          <label class="w-full">
-            <p class="">Confirmar contraseña</p>
+    <Dialog.Content
+      class="bg-white flex flex-col justify-center items-center w-10/12 rounded-xl border-0"
+      style="background: url('/images/FONDOS/FONDO.webp') no-repeat;"
+    >
+      <Dialog.Header>
+        <Dialog.Title class="font-normal text-center">
+          <p class="text-agreen f6-latino">
+            {register ? "Registro" : "Iniciar sesión"}
+          </p>
+        </Dialog.Title>
+        <Dialog.Description class="Description">
+          {#if error}
+            <p class="text-ared f10-latino">⚠️ {errorMsg}</p>
+          {/if}
+        </Dialog.Description>
+      </Dialog.Header>
+
+      <div class="w-11/12 md:w-8/12">
+        <form class="flex flex-col space-y-2 items-center">
+          <label class="w-full text-agreen f10-latino">
+            <p class="text-agreen f10-latino">Correo electrónico</p>
             <input
-              bind:value={confirmPass}
-              type="password"
-              placeholder="Contraseña"
-              class="bg-white rounded-full px-3 py-1 w-full focus:border-ared focus:ring-ared"
+              bind:value={email}
+              type="email"
+              placeholder="Correo electrónico"
+              class="vinput focus:ring-2 focus:ring-ared focus:text-agreen focus:bg-gray-200"
             />
           </label>
-        {/if}
 
-        <button
-          on:click={handleAuthenticate}
-          type="submit"
-          class="min-w-36 rounded-xl mt-5 px-5 py-2 text-white bg-ared flex justify-center items-center transition-all ease-linear duration-200"
-        >
-          {#if authenticating}
-            <div class="loader"></div>
-          {:else}
-            <p>{register ? "Registrarme" : "Iniciar sesión"}</p>
+          <label class="w-full text-agreen f10-latino">
+            <p class="text-agreen f10-latino">Contraseña</p>
+            <input
+              bind:value={password}
+              type="password"
+              placeholder="Contraseña"
+              class="vinput focus:ring-2 focus:ring-ared focus:text-agreen focus:bg-gray-200"
+            />
+          </label>
+          {#if register}
+            <label class="w-full text-agreen f10-latino">
+              <p>Confirmar Contraseña</p>
+              <input
+                bind:value={confirmPass}
+                type="password"
+                placeholder="Contraseña"
+                class="vinput focus:ring-2 focus:ring-ared focus:text-agreen focus:bg-gray-200"
+              />
+            </label>
           {/if}
-        </button>
-      </form>
 
-      <!--  -->
-      <div class="flex flex-col items-center text-agreen mt-5">
-        {#if register}
-          <div class="flex">
-            <p>
-              ¿Ya tienes una cuenta?
-              <button class="underline" on:click={handleRegister}>
-                Inicia sesión
-              </button>
-            </p>
-          </div>
-        {:else}
-          <div class="flex">
-            <p>
-              ¿No tienes una cuenta?
-              <button class="underline" on:click={handleRegister}>
-                Regístrate
-              </button>
-            </p>
-          </div>
-        {/if}
+          <button on:click={handleAuthenticate} type="submit" class="btnp">
+            {#if authenticating}
+              <div class="loader"></div>
+            {:else}
+              <p>{register ? "Registrarme" : "Iniciar sesión"}</p>
+            {/if}
+          </button>
+        </form>
+
+        <!--  -->
+        <div class="flex flex-col items-center text-agreen mt-5">
+          {#if register}
+            <div class="flex">
+              <p>
+                ¿Ya tienes una cuenta?
+                <button class="underline" on:click={handleRegister}>
+                  Inicia sesión
+                </button>
+              </p>
+            </div>
+          {:else}
+            <div class="flex">
+              <p>
+                ¿No tienes una cuenta?
+                <button class="underline" on:click={handleRegister}>
+                  Regístrate
+                </button>
+              </p>
+            </div>
+          {/if}
+        </div>
       </div>
-    </div>
-
-    <!-- <Dialog.Footer>
-      <Button type="submit">Save changes</Button>
-    </Dialog.Footer> -->
-  </Dialog.Content>
-</Dialog.Root>
-
-<style>
-  .loader {
-    width: 20px;
-    aspect-ratio: 1;
-    position: relative;
-  }
-  .loader:before,
-  .loader:after {
-    content: "";
-    position: absolute;
-    inset: 0;
-    background: #ffb940;
-    box-shadow: 0 0 0 50px;
-    clip-path: polygon(
-      50% 0%,
-      61% 35%,
-      98% 35%,
-      68% 57%,
-      79% 91%,
-      50% 70%,
-      21% 91%,
-      32% 57%,
-      2% 35%,
-      39% 35%
-    );
-  }
-  .loader:after {
-    animation: l5 1s infinite;
-    transform: perspective(300px) translateZ(0px);
-  }
-  @keyframes l5 {
-    to {
-      transform: perspective(300px) translateZ(150px);
-      opacity: 0;
-    }
-  }
-</style>
+    </Dialog.Content>
+  </Dialog.Root>
+</div>
