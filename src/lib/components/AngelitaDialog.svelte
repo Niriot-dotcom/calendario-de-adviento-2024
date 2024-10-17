df5d8b073f948348f67123d35ba725adeaa52bb3
Paty Lopez <lopezpatyyarely@gmail.com>
2024-10-17 12:46:16 -0600

    init AngelitaDialog with custom dialogs.

--- src/lib/components/AngelitaDialog.svelte
+++ src/lib/components/AngelitaDialog.svelte
@@ -1,79 +1,106 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { AuthStore } from "../../stores/AuthStore";
+
+  let dialog: string = "";
+  let isLargeText: boolean = false;
+  let currentDay: number = 0;
+
+  AuthStore.subscribe((curr) => {
+    currentDay = curr?.currentDay;
+    let lastDay = curr?.data.lastDay;
+    if (lastDay === currentDay) {
+      dialog = `Recuerda realizar tu buena acción del día y regresar mañana`;
+      isLargeText = true;
+    } else if (lastDay < currentDay) {
+      dialog = `Abre todas<br />las puertas<br />hasta hoy`;
+      isLargeText = true;
+    }
+  });
 </script>
 
-<!-- <div
-  class="absolute top-10 right-10 z-0 flex justify-end w-full h-full animate-bounce-smooth"
->
-  GLOBO ANGELITA
-  <div class="w-fit h-[15vh] relative">
-    <div class="w-fit h-[15vh]">
-      <img
-        class="w-fit h-full object-contain"
-        alt="GLOBO PERSONAJES"
-        src="/images/ELEMENTOS/GLOBO ANGELITA.webp"
-      />
-
-      <div
-        class="absolute top-[1.5vh] px-[1.5vh] left-0 w-full h-[9vh] flex flex-col justify-center text-ared overflow-hidden"
-      >
-        <p class="leading-none f10-latino">¡Hola!</p>
-        <p class="leading-none f10-latino">Bienvenido<br />al día 1</p>
-      </div>
-    </div>
-  </div>
-
-  ANGELITA
-  <div class="w-fit h-2/6">
-    <img
-      class="w-5/6 h-full object-contain"
-      alt="ANGELITA"
-      src="/images/PERSONAJES/ANGELITA.webp"
-    />
-  </div>
-</div> -->
-
 <!-- GLOBO ANGELITA LARGO -->
-<div
-  class="absolute top-[15vh] right-[3vw] z-0 flex justify-end w-full h-full animate-bounce-smooth"
->
-  <div class="w-fit h-[30vh] relative">
-    <div class="w-full h-full">
-      <img
-        class="w-fit h-full object-contain"
-        alt="GLOBO PERSONAJES"
-        src="/images/ELEMENTOS/GLOBO ANGELITA LARGO.webp"
-      />
+{#if isLargeText}
+  <div
+    class="absolute top-[15vh] right-[3vw] z-0 flex justify-end w-full h-full animate-bounce-smooth"
+  >
+    <div class="w-fit h-[30vh] relative">
+      <div class="w-full h-full">
+        <img
+          class="w-fit h-full object-contain"
+          alt="GLOBO PERSONAJES"
+          src="/images/ELEMENTOS/GLOBO ANGELITA LARGO.webp"
+        />
 
-      <div
-        class="absolute top-[3vh] px-[3vh] left-0 w-full h-[20vh] flex flex-col justify-center text-ared overflow-hidden border border-red-400"
-      >
-        <p
-          class="leading-none f10-latino border border-red-400 h-full flex items-center"
+        <div
+          class="absolute top-[3vh] px-[3vh] left-0 w-full h-[20vh] flex flex-col items-center justify-center text-ared overflow-hidden"
         >
-          <!-- Realiza las buenas acciones diarias durante el Adviento para ayudarme
+          <p class="leading-none f9-latino items-center mb-2">
+            ¡Hola!<br />
+            <span>Hoy es el día {currentDay}</span>
+          </p>
+          <p class="leading-none f10-latino flex items-center">
+            <!-- Realiza las buenas acciones diarias durante el Adviento para ayudarme
           a regresar a casa con {$AuthStore.data.angelitaName} -->
-          <!-- Da click sobre el día correspondiente y comencemos esta aventura -->
-          <!-- Recuerda realizar tu buena acción del día y regresar mañana-->
-          <!-- Con tu ayuda cada vez falta menos para regresar a casa con {$AuthStore
+            <!-- Da click sobre el día correspondiente y comencemos esta aventura -->
+            <!-- Recuerda realizar tu buena acción del día y regresar mañana-->
+            <!-- Con tu ayuda cada vez falta menos para regresar a casa con {$AuthStore
             .data.angelitoName} -->
-          ¡Lo lograste!<br />Me ayudaste a volver a casa mientras realizabas
-          acciones nobles, jugando y disfrutando del tiempo con tus seres
-          <!-- queridos, recuerda que la familia es el mejor regalo. -->
-        </p>
+            <!-- ¡Lo lograste!<br />Me ayudaste a volver a casa mientras realizabas
+          acciones nobles, jugando y disfrutando del tiempo con tus seres -->
+            <!-- queridos, recuerda que la familia es el mejor regalo. -->
+            {@html dialog}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
 
-  <!-- ANGELITA -->
-  <div class="w-fit h-2/6 mt-[8vh]">
-    <img
-      class="w-5/6 h-full object-contain"
-      alt="ANGELITA"
-      src="/images/PERSONAJES/ANGELITA.webp"
-    />
+    <!-- ANGELITA -->
+    <div class="w-fit h-2/6 mt-[8vh]">
+      <img
+        class="w-5/6 h-full object-contain"
+        alt="ANGELITA"
+        src="/images/PERSONAJES/ANGELITA.webp"
+      />
+    </div>
   </div>
-</div>
+{:else}
+  <div
+    class="absolute top-10 right-10 z-0 flex justify-end w-full h-full animate-bounce-smooth"
+  >
+    <!-- GLOBO ANGELITA -->
+    <div class="w-fit h-[15vh] relative">
+      <div class="w-fit h-[15vh]">
+        <img
+          class="w-fit h-full object-contain"
+          alt="GLOBO PERSONAJES"
+          src="/images/ELEMENTOS/GLOBO ANGELITA.webp"
+        />
+
+        <div
+          class="absolute top-[1.5vh] px-[1.5vh] left-0 w-full h-[9vh] flex flex-col justify-center text-ared overflow-hidden"
+        >
+          <!-- <p class="leading-none f10-latino">¡Hola!</p>
+        <p class="leading-none f10-latino">Bienvenido<br />al día 1</p> -->
+          <p class="leading-none f9-latino items-center mb-2">
+            ¡Hola!<br />
+            <span>Hoy es el día {currentDay}</span>
+          </p>
+          <p class="leading-none f10-latino">{@html dialog}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- ANGELITA -->
+    <div class="w-fit h-2/6">
+      <img
+        class="w-5/6 h-full object-contain"
+        alt="ANGELITA"
+        src="/images/PERSONAJES/ANGELITA.webp"
+      />
+    </div>
+  </div>
+{/if}
 
 <!-- ANGELITO -->
 <div
